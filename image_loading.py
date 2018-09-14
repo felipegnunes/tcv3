d246798b7523b1148925084bddd310cf4a4ba368
Felipe <felipeguimaraesnunes@gmail.com>
2018-09-14 09:23:35 -0300

    Refactoring

--- image_loading.py
+++ image_loading.py
@@ -13,36 +13,58 @@ def get_filenames(files_path):
 	filenames = [f for f in os.listdir(files_path) if os.path.isfile(os.path.join(files_path, f))]
 	return filenames
 
-def get_file_number(directory_path):
-	return len(get_filenames(directory_path))
-
 def load_filenames(dataset_directory):
-	train_dir = os.path.join(dataset_directory, 'train')
-	test_dir
+	train_dir_path = os.path.join(dataset_directory, 'train')
+	test_dir_path = os.path.join(dataset_directory, 'test')
+	
+	train_directories = get_immediate_subdirectories(train_dir_path)
+	test_directories = get_immediate_subdirectories(test_dir_path)
+	
+	train_images = []
+	
+	for directory in train_directories:
+		directory_path = os.path.join(train_dir_path, directory)
+		label = int(directory)
+		image_list = get_filenames(directory_path)
+	
+		for image in image_list:
+			image_path = os.path.join(directory_path, image)
+			train_images.append((image_path, label))
+	
+	test_images = get_filenames(test_dir_path)
+	
+	return train_images, test_images		
 
 def load_images():
-	pass
+	example_img_path = imgs[0][0]
+	example_image = cv2.imread(example_img_path, cv2.IMREAD_UNCHANGED)
+	
+	height = example_image.shape[0] 
+	width = example_image.shape[1]
+	if (len(example_image.shape) < 3):
+		num_channels = 1
+	else:
+		num_channels = example_image.shape[2]
+	
+	X = np.empty([num_train_imgs, height, width, num_channels], dtype=np.uint8)
+	y = np.empty([num_train_imgs], dtype=np.int64)
+	
+	for image_path, label in imgs:	
+		X_train[i] = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE).reshape(height, width, num_channels)
+		y_train[i] = label
+		i += 1
+	
+	return X, y
 	
 def main():
 	# PARAMETERS
-	train_dir = sys.argv[1] #'/tmp/guest-jbsthn/topicos_compvis3/data_part1/train'
-	test_dir = sys.argv[2] #'/tmp/guest-jbsthn/topicos_compvis3/data_part1/test'
-	train_test_rate = float(sys.argv[3]) #0.7
+	dataset_dir = sys.argv[1]
+	train_test_rate = float(sys.argv[2])
 	
 	# LOADING IMAGE NAMES AND LABELS
 	
-	directory_list = get_immediate_subdirectories(train_dir)
-
-	imgs = []
-
-	for directory in directory_list:
-		directory_path = os.path.join(train_dir, directory)
-		label = int(directory)
-		image_list = get_filenames(directory_path)
-		
-		for image in image_list:
-			image_path = os.path.join(directory_path, image)
-			imgs.append((image_path, label))
+	imgs, test_imgs = load_filenames(dataset_dir)
+	print(len(test_imgs))
 	
 	# SHUFFLING IMAGES
 		

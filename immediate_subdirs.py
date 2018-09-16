e1e336b35f9a8d71048f2d66c41474e5cfacba65
Felipe <felipeguimaraesnunes@gmail.com>
2018-09-15 21:22:32 -0300

    Linear Regression Toy Problem

--- immediate_subdirs.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def get_immediate_subdirectories(directory_path):
-	return [sub_directory for sub_directory in os.listdir(directory_path) 
-		if os.path.isdir(os.path.join(directory_path, sub_directory))]
-		
-print(get_immediate_subdirectories('/home/felipe'))

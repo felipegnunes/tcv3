69fdadacb95844036f64013f79d982afdca8c7ee
Felipe <felipeguimaraesnunes@gmail.com>
2018-11-14 21:30:04 -0300

    Model

--- trabalho3/main.py
+++ trabalho3/main.py
@@ -20,40 +20,40 @@ class Model:
 		
 		self.graph = tf.Graph()
 		with self.graph.as_default():
+			self.global_step = tf.Variable(0)
 			self.X = tf.placeholder(tf.float32, shape = (None, self.img_height, self.img_width, self.img_num_channels), name = 'X')
 			self.y = tf.placeholder(tf.int32, shape = (None, ), name = 'y')
 			self.y_one_hot = tf.one_hot(self.y, num_classes)
-			self.learning_rate = tf.placeholder(tf.float32, name = 'learning_rate')
-			self.keep_prob0 = tf.placeholder_with_default(1.0, shape = ())
-			self.keep_prob1 = tf.placeholder_with_default(1.0, shape = ())
-			self.keep_prob2 = tf.placeholder_with_default(1.0, shape = ())
+			self.learning_rate = tf.train.exponential_decay(learning_rate = 1e-3, global_step = self.global_step, decay_steps = (4000//batch_size) * 20, decay_rate = 0.95, staircase = True, name = 'learning_rate')
+			#self.learning_rate = tf.placeholder(tf.float32, name = 'learning_rate')
 			
-			self.output = tf.layers.conv2d(self.X, filters = 32, kernel_size = (3, 3), strides = (1, 1), padding = 'same', activation = tf.nn.relu)
-			self.output = tf.layers.conv2d(self.output, filters = 32, kernel_size = (3, 3), strides = (3, 3), padding = 'same', activation = tf.nn.relu)
-			self.output = tf.layers.max_pooling2d(self.output, pool_size = (2, 2), strides = (1, 1), padding = 'same')
-			self.output = tf.nn.dropout(self.output, self.keep_prob0)			
+			self.output = tf.layers.conv2d(self.X, filters = 32, kernel_size = (5, 5), strides = (1, 1), padding = 'same', activation = tf.nn.relu)
+			self.output = tf.layers.conv2d(self.output, filters = 32, kernel_size = (5, 5), strides = (1, 1), padding = 'same', activation = tf.nn.relu)
+			self.output = tf.layers.max_pooling2d(self.output, pool_size = (2, 2), strides = (2, 2), padding = 'same')
+			self.output = tf.nn.dropout(self.output, 0.75)			
 			print(self.output.shape)
 			
-			self.output = tf.layers.conv2d(self.output, filters = 64, kernel_size = (2, 2), strides = (1, 1), padding = 'same', activation = tf.nn.relu)
-			self.output = tf.layers.conv2d(self.output, filters = 64, kernel_size = (2, 2), strides = (2, 2), padding = 'same', activation = tf.nn.relu)
+			self.output = tf.layers.conv2d(self.output, filters = 64, kernel_size = (3, 3), strides = (1, 1), padding = 'same', activation = tf.nn.relu)
+			self.output = tf.layers.conv2d(self.output, filters = 64, kernel_size = (3, 3), strides = (1, 1), padding = 'same', activation = tf.nn.relu)
 			self.output = tf.layers.max_pooling2d(self.output, pool_size = (2, 2), strides = (2, 2), padding = 'same')
-			self.output = tf.nn.dropout(self.output, self.keep_prob1)
+			self.output = tf.nn.dropout(self.output, 0.75)
 			print(self.output.shape)
 			
 			num_elements = (self.output.shape[1] * self.output.shape[2] * self.output.shape[3])
 			print('Input size of the dense layer: {}'.format(num_elements))
 			self.output = tf.reshape(self.output, (-1, num_elements))
 			
-			self.output = tf.layers.dense(self.output, 128, activation = tf.nn.relu)
-			self.output = tf.nn.dropout(self.output, self.keep_prob2)
-			
+			#self.output = tf.layers.dense(self.output, 512, activation = tf.nn.relu)
+			#self.output = tf.nn.dropout(self.output, 0.5)
+			self.output = tf.layers.dense(self.output, 256, activation = tf.nn.relu)
+			self.output = tf.nn.dropout(self.output, 0.5)
 			self.output = tf.layers.dense(self.output, num_classes, activation = tf.nn.softmax)
 			
 			self.result = tf.argmax(self.output, 1, output_type = tf.int32)
 			self.loss = tf.losses.softmax_cross_entropy(self.y_one_hot, self.output)
 			self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.result, self.y), tf.float32), name = 'accuracy')
 			
-			self.train_operation = tf.train.AdamOptimizer(learning_rate = self.learning_rate).minimize(self.loss)
+			self.train_operation = tf.train.AdamOptimizer(learning_rate = self.learning_rate).minimize(self.loss, global_step = self.global_step)
 			self.saver = tf.train.Saver()
 			
 			if first_run:
@@ -78,15 +78,11 @@ class Model:
 				num_batches = 0
 				
 				print('Epoch {}'.format(epoch))
-				
+				print('Learning Rate: {}'.format(session.run(self.learning_rate)))
 				while index < num_samples:
 					batch_loss, batch_accuracy, _ = session.run([self.loss, self.accuracy, self.train_operation], 
 										    feed_dict = {self.X: X_train[index : index + self.batch_size], 
-												 self.y: y_train[index : index + self.batch_size],
-												 self.keep_prob0: 0.75,
-												 self.keep_prob1: 0.75,
-												 self.keep_prob2: 0.5,
-												 self.learning_rate: 1e-3})
+												 self.y: y_train[index : index + self.batch_size]})
 					training_loss += batch_loss
 					training_accuracy += batch_accuracy
 					num_batches += 1
@@ -106,13 +102,7 @@ class Model:
 				if (X_remainder.shape[0] > 0):
 					X_remainder = np.concatenate((X_remainder, X_train[ : index]), axis = 0)
 					y_remainder = np.concatenate((y_remainder, y_train[ : index]), axis = 0)	
-					batch_loss, batch_accuracy, _ = session.run([self.loss, self.accuracy, self.train_operation], 
-										    feed_dict = {self.X: X_remainder,
-										    		 self.y: y_remainder,
-										    		 self.keep_prob0: 0.75,
-												 self.keep_prob1: 0.75,
-												 self.keep_prob2: 0.5,
-										    		 self.learning_rate: 1e-3})
+					batch_loss, batch_accuracy, _ = session.run([self.loss, self.accuracy, self.train_operation], feed_dict = {self.X: X_remainder, self.y: y_remainder})
 					training_loss += batch_loss
 					training_accuracy += batch_accuracy
 					num_batches += 1
@@ -125,10 +115,9 @@ class Model:
 				validation_accuracy = 0
 				num_val_batches = 0
 				for i in range(0, num_validation_samples, self.batch_size):
-					val_batch_loss, val_batch_acc = session.run([self.loss, self.accuracy], 
-										    feed_dict = {self.X: X_validation[i : min(i + self.batch_size, num_validation_samples)], 
-										    		 self.y: y_validation[i : min(i + self.batch_size, num_validation_samples)]
-										    		})
+					val_batch_loss, val_batch_acc = session.run([self.loss, self.accuracy], feed_dict = {self.X: X_validation[i : min(i + self.batch_size, num_validation_samples)], 
+										    		 			     self.y: y_validation[i : min(i + self.batch_size, num_validation_samples)]
+										    		 			    })
 					validation_loss += val_batch_loss
 					validation_accuracy += val_batch_acc				
 					num_val_batches += 1
@@ -222,8 +211,8 @@ def main():
 	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 	DATASET_DIRECTORY = '/home/felipe/tcv3/data_part1'
 	TRAIN_RATE = 0.8
-	NUM_EPOCHS = 30
-	BATCH_SIZE = 500
+	NUM_EPOCHS = 200
+	BATCH_SIZE = 250
 	
 	X, y, X_hidden = dataset_manip.load_dataset(DATASET_DIRECTORY)
 	num_classes = len(set(y))
@@ -243,9 +232,9 @@ def main():
 	
 	X, X_test, y, y_test = dataset_manip.split_dataset(X, y, rate = 0.8)
 	
-	ensemble = Ensemble(input_shape = X.shape[1: ], num_classes = num_classes, num_models = 20, batch_size = 400)
+	ensemble = Ensemble(input_shape = X.shape[1: ], num_classes = num_classes, num_models = 5, batch_size = 250)
 	print(ensemble.measure_accuracy(X_test, y_test))
-	ensemble.train(X = X, y = y, epochs_per_model = 150, train_rate = 0.8)
+	ensemble.train(X = X, y = y, epochs_per_model = 50, train_rate = 0.8)
 	print(ensemble.measure_accuracy(X_test, y_test))
 	
 	for i, model in enumerate(ensemble.models):

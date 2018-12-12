6eb8ef55398ff45421c08176f5309f0b8972e492
Felipe <felipeguimaraesnunes@gmail.com>
2018-12-12 19:02:49 -0300

    GAN

--- trabalho5/train.py
+++ trabalho5/train.py
@@ -42,10 +42,10 @@ is_training = tf.placeholder(tf.bool, name = 'is_training');
 def discriminator(X, reuse = None):
 	with tf.variable_scope('discriminator', reuse = reuse):
 		fc1 = tf.reshape(tensor = X, shape = (-1, 1024))
-		fc1 = tf.layers.dense(inputs = fc1, units = 256, use_bias = True)
+		fc1 = tf.layers.dense(inputs = fc1, units = 256, use_bias = False)
 		fc1 = tf.nn.leaky_relu(features = fc1, alpha = 0.01)
 		
-		fc2 = tf.layers.dense(inputs = fc1, units = 64, use_bias = True)
+		fc2 = tf.layers.dense(inputs = fc1, units = 128, use_bias = False)
 		fc2 = tf.nn.leaky_relu(features = fc2, alpha = 0.01)
 		
 		fc3 = tf.layers.dense(inputs = fc2, units = 1)
@@ -54,18 +54,26 @@ def discriminator(X, reuse = None):
 		
 		return fc3
 		
-		#conv1 = tf.layers.conv2d(inputs = X, filters = 4, kernel_size = (3, 3), strides = (2, 2), padding = 'same') 
+		#print(X.shape)
+		#conv1 = tf.layers.conv2d(inputs = X, filters = 8, kernel_size = (3, 3), strides = (2, 2), padding = 'same', use_bias = False) 
 		#conv1 = tf.layers.batch_normalization(conv1, training = is_training)
 		#conv1 = tf.nn.leaky_relu(features = conv1, alpha = 0.01)
+		#print(conv1.shape)
 		
-		#conv2 = tf.layers.conv2d(inputs = conv1, filters = 8, kernel_size = (3, 3), strides = (2, 2), padding = 'same', use_bias = False)
+		#conv2 = tf.layers.conv2d(inputs = conv1, filters = 16, kernel_size = (2, 2), strides = (2, 2), padding = 'same', use_bias = False)
+		#conv2 = tf.layers.batch_normalization(conv2, training = is_training)
 		#conv2 = tf.nn.leaky_relu(features = conv2, alpha = 0.01)
+		#print(conv2.shape)
 		
-		#conv3 = tf.layers.conv2d(inputs = conv2, filters = 16, kernel_size = (3, 3), strides = (2, 2), padding = 'same')
+		#conv3 = tf.layers.conv2d(inputs = conv2, filters = 32, kernel_size = (2, 2), strides = (2, 2), padding = 'same', use_bias = False)
+		#conv3 = tf.layers.batch_normalization(conv3, training = is_training)
 		#conv3 = tf.nn.leaky_relu(features = conv3, alpha = 0.01)
+		#print(conv3.shape)
 		
-		#fc1 = tf.reshape(tensor = conv3, shape = (-1, conv2.shape[1] * conv2.shape[2] * conv2.shape[3]))
+		#fc1 = tf.reshape(tensor = conv3, shape = (-1, conv3.shape[1] * conv3.shape[2] * conv3.shape[3]))
 		#fc1 = tf.layers.dense(inputs = fc1, units = 1)
+		#print(fc1.shape)
+		#print()
 		
 		#return fc1
 
@@ -73,48 +81,49 @@ def discriminator(X, reuse = None):
 
 def generator(z, reuse = None):
 	with tf.variable_scope('generator', reuse = reuse):	
-		fc1 = tf.layers.dense(inputs = z, units = 256, use_bias = False)
-		#fc1 = tf.layers.batch_normalization(fc1, training = is_training)
+		fc1 = tf.layers.dense(inputs = z, units = 128, use_bias = False)
+		fc1 = tf.layers.batch_normalization(fc1, training = is_training)
 		fc1 = tf.nn.leaky_relu(features = fc1, alpha = 0.01)
 		print(fc1.shape)
 		
-		fc2 = tf.layers.dense(inputs = fc1, units = 512, use_bias = False)
-		#fc2 = tf.layers.batch_normalization(fc2, training = is_training)
+		fc2 = tf.layers.dense(inputs = fc1, units = 256, use_bias = False)
+		fc2 = tf.layers.batch_normalization(fc2, training = is_training)
 		fc2 = tf.nn.leaky_relu(features = fc2, alpha = 0.01)
 		print(fc2.shape)
 		
-		fc3 = tf.layers.dense(inputs = fc2, units = 1024, use_bias = False)
-		#fc3 = tf.layers.batch_normalization(fc3, training = is_training)
-		fc3 = tf.reshape(tensor = fc3, shape = (-1, 32, 32, 1))
+		fc3 = tf.layers.dense(inputs = fc2, units = 512, use_bias = False)
+		fc3 = tf.layers.batch_normalization(fc3, training = is_training)
+		fc3 = tf.nn.leaky_relu(features = fc3, alpha = 0.01)
 		print(fc3.shape)
+		
+		fc4 = tf.layers.dense(inputs = fc3, units = 1024, use_bias = False)
+		fc4 = tf.reshape(tensor = fc4, shape = (-1, 32, 32, 1))
+		print(fc4.shape)
 		print()
 		
-		return tf.sigmoid(fc3)
+		return tf.sigmoid(fc4)
 
 ### DEFINING MODEL ###
 
 G = generator(z)
 D_fake = discriminator(G)
-D_real = discriminator(X_real, reuse = True)
 
-### LOSSES ###
+G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_fake, labels = tf.ones_like(D_fake)))
+
+with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+	g_vars = [var for var in tf.trainable_variables() if 'generator' in var.name]
+	G_trainer = tf.train.AdamOptimizer(1e-4).minimize(G_loss, var_list = g_vars)
+
+D_real = discriminator(X_real, reuse = True)
 
 D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_real, labels = tf.ones_like(D_real) * 0.9))
 D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_fake, labels = tf.zeros_like(D_fake)))
 
 D_loss = D_loss_real + D_loss_fake
-G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_fake, labels = tf.ones_like(D_fake)))
 
-### TRAINERS ###
-
-tvars = tf.trainable_variables()
-d_vars = [var for var in tvars if 'discriminator' in var.name]
-g_vars = [var for var in tvars if 'generator' in var.name]
-
-update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-with tf.control_dependencies(update_ops):	
-	D_trainer = tf.train.AdamOptimizer(5e-4).minimize(D_loss, var_list = d_vars)
-	G_trainer = tf.train.AdamOptimizer(1e-2).minimize(G_loss, var_list = g_vars)
+with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):	
+	d_vars = [var for var in tf.trainable_variables() if 'discriminator' in var.name]	
+	D_trainer = tf.train.AdamOptimizer(1e-4).minimize(D_loss, var_list = d_vars)
 
 ### TRAINING ###
 
@@ -128,12 +137,14 @@ with tf.Session() as sess:
 		for i in range(0, NUM_SAMPLES, BATCH_SIZE):
 				j = min(i + BATCH_SIZE, NUM_SAMPLES)
 				
-				z_batch = np.array([np.random.normal(0, 1, size = (64, )) for k in range(BATCH_SIZE)])
+				z_batch = np.random.normal(0, 1, size = (BATCH_SIZE, 64))
 				X_batch = X[i : j]
 				
 				_, loss_dis = sess.run([D_trainer, D_loss], feed_dict = {X_real: X_batch, z: z_batch, is_training: True})
 				
-				z_batch = np.array([np.random.normal(0, 1, size = (64, )) for k in range(BATCH_SIZE)])
+				z_batch = np.random.normal(0, 1, size = (BATCH_SIZE, 64))
+				_, loss_gen = sess.run([G_trainer, G_loss], feed_dict = {z: z_batch, is_training: True})
+				z_batch = np.random.normal(0, 1, size = (BATCH_SIZE, 64))
 				_, loss_gen = sess.run([G_trainer, G_loss], feed_dict = {z: z_batch, is_training: True})
 				
 				print('Loss Discriminator: {}'.format(loss_dis))
@@ -154,7 +165,7 @@ with tf.Session() as sess:
 		#gen_sample /= np.amax(gen_sample)
 		gen_sample *= 255
 		
-		cv2.imwrite('/home/felipe/tcv3/trabalho5/results/' + str(epoch) + '.png', gen_sample)	
+		cv2.imwrite('/home/nunes/tcv3/trabalho5/results/' + str(epoch) + '.png', gen_sample)	
 	
 ### SAVING ###
 
